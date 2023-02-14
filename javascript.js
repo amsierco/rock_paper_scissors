a00fefbbbaa860a87b4995561ba76589fdbe4ab5
Austin <75064473+amsierco@users.noreply.github.com>
2023-02-14 18:45:26 -0500

    Modified all files. Basic RPS Game completed.

--- javascript.js
+++ javascript.js
@@ -1,18 +1,26 @@
-//Assign button listeners
-var buttons = document.querySelectorAll('.choice-container .btn');
-var userChoice = "";
+//Variables
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+//const body = document.getElementsByTagName("BODY")[0];
+var game_count = 0;
+var player_wins = 0;
 
-//const btns = document.getElementsByClassName(".choice-container .btn");
-//console.log(btns.item);
+var score = document.getElementById("score-id");
 
-for(i of buttons){
-    i.addEventListener('click', function(e){
 
-        userChoice = this.getAttribute('id');
-        playGame(this.getAttribute('id'));
-    });
-}
+//Button events
+rock.addEventListener('click', function(e) {
+  playGame(this.getAttribute('id'), this);
+});
+paper.addEventListener('click', function(e) {
+  playGame(this.getAttribute('id'), this);
+});
+scissors.addEventListener('click', function(e) {
+  playGame(this.getAttribute('id'), this);
+});
 
+//Random computer choice
 function computerChoice(){
     switch(Math.floor(Math.random() * 3)){
         case 0:
@@ -28,11 +36,7 @@ function computerChoice(){
 }
 
 //Returns winner
-function compareInput(user){
-
-    var computer = computerChoice();
-
-    console.log("Comp: "+computer+" User: "+user);
+function compareInput(user, computer){
 
     if(user == computer){
         return "draw";
@@ -46,14 +50,52 @@ function compareInput(user){
 }
 
 //Main game function
-function playGame(userChoice){
-    if(compareInput(userChoice) == "draw"){
-        console.log("Draw!");
-    } else if(compareInput(userChoice) == "player"){
-        console.log("Player Wins!");
-    } else if(compareInput(userChoice) == "computer") {
-        console.log("Computer Wins!");
-    } else {
-        console.log("Error!");
-    }
+function playGame(userChoice, obj){
+//console.log(obj.getAttribute('id'));
+	if(game_count < 10000){
+      let computer = computerChoice();
+      if(compareInput(userChoice, computer) == "draw"){
+          //console.log("Draw!");
+          //body.style.backgroundColor = 'gray';
+          obj.style.backgroundColor = 'gray';
+      } else if(compareInput(userChoice, computer) == "player"){
+          //console.log("Player Wins!");
+          player_wins++;
+          //body.style.backgroundColor = 'green';
+          obj.style.backgroundColor = 'green';
+      } else if(compareInput(userChoice, computer) == "computer") {
+          //console.log("Computer Wins!");
+          //body.style.backgroundColor = 'red';
+          obj.style.backgroundColor = 'red';
+      } else {
+          //console.log("Error!");
+          //body.style.backgroundColor = 'purple';
+      }
+      
+      let lose_count = (game_count-player_wins);
+      let percentage = 0;
+      if(lose_count < 0){
+      	lose_count = 0;
+      }
+      if(game_count != 0){
+      	percentage = (((player_wins/game_count).toFixed(2))*100);
+      }
+ 			score.innerHTML = "Wins: " + player_wins + " Loses: " + lose_count + " Win Percentage: " + percentage + "%";	
+      game_count++;
+      
+      //Resets the button's color
+      setTimeout(() => {
+				obj.style.backgroundColor = 'black';
+      }, 1000);
+	} else {
+  	console.log("You won: " + player_wins + " games!");
+  }
 }
+
+//Restart button
+const restart = document.getElementById('restart-id');
+restart.addEventListener('click', function(e){
+	game_count = 0;
+  player_wins = 0;
+  score.innerHTML = "Wins: 0 Loses: 0 Win Percentage: 0%";
+});
